5fa9c2db5fdd3200975dd1e03bf091bcf3b49c3a
HarshadaG13 <harshagadakh@gmail.com>
2022-06-05 12:05:16 +0530

    branch 'issue1' first commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 
 public class Main {
+	public static Greet() {
+		System.out.println("Good morning");
+	}
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		System.out.println("This is the main class");
+		
+		
 
 	}
 
